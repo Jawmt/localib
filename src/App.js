5dcc4ab4213f65c7d90befa8938073c3cb94552f
Jawad MB <jawad.mrabet@labom2iformation.fr>
2022-10-13 09:35:39 +0200

    Mise en place du Router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Header from './components/Header';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Locataire from './pages/Locataire';
 
 function App() {
-  return (
-    <Header/>
+  return ( 
+    <>
+      <Header/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/locataire" element={<Locataire/>} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
---
+++ src/pages/Locataire.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Locataire = () => {
+  return (
+    <div>Locataire</div>
+  )
+}
+
+export default Locataire
